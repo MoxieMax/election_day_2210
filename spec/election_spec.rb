9ec32e768d31b5ee0e7bc4c73852edb8493072d6
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-01 09:59:44 -0700

    Created testing for election covering most of iteration 3

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -6,6 +6,8 @@ require './lib/election'
 RSpec.describe Election do
   describe 'Iteration 3' do
     let (:election) {Election.new('2022')}
+    let (:race1) {Race.new("Virginia District 4 Representative")}
+    let (:race2) {Race.new("Texas Governor")}
     
     it 'exists' do
       expect(election).to be_an(Election)
@@ -14,6 +16,23 @@ RSpec.describe Election do
     it 'has readable attributes' do
       expect(election.year).to eq('2022')
       expect(election.races).to eq([])
+    end
+    
+    xit 'can add races' do
+      election.add_race(race1)
+      election.add_race(race2)
+      
+      expect(election.races).to eq([race1, race2])
+    end
+    
+    xit 'can add candidates to races' do
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+      
+      expect(election.candidates).to eq([candidate1, candidate2, candidate3, candidate4, candidate5])
     end
   end
 end
