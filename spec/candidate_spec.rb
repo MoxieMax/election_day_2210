3dc7fd0a20902f59d7b465dac44aaf17a892a589
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-01 08:31:24 -0700

    Add testing for iteration 1 of candidate class

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,26 @@
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  describe 'Iteration 1' do
+    let (:diana) {Candidate.new({name: "Diana D", party: :democrat})}
+    it 'exists' do
+      expect(diana).to be_a(Candidate)
+    end
+    
+    it 'has readable attributes' do
+      expect(diana.name).to eq("Diana D")
+      expect(diana.party).to eq(:democrat)
+    end
+    
+    xit 'can count votes' do
+      expect(diana.votes).to eq(0)
+      diana.vote_for!
+      diana.vote_for!
+      diana.vote_for!
+      expect(diana.votes).to eq(3)
+      diana.vote_for!
+      expect(diana.votes).to eq(4)
+    end
+      
+  end
+end
